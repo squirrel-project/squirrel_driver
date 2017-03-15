a2a167be51746a583e0bea445fc63c3f73e0366c
ipa-nhg <nhg@ipa.fhg.de>
2017-03-15 17:23:01 +0100

    test travis config

--- .travis.install.sh
+++ .travis.install.sh
@@ -5,4 +5,4 @@ echo "BEFORE INSTALL IS RUNNING"
 sudo echo "deb http://doc.openrobotino.org/download/packages/amd64 ./" | sudo tee -a /etc/apt/sources.list
 sudo apt-get update
 sudo apt-key update
-sudo apt-get install -qq -y robotino-api2
+sudo apt-get install -qq -y --force-yes robotino-api2
