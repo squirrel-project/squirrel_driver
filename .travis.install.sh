bc5aef7c09fc21da8518c363b09c58e509bb6a5d
ipa-nhg <nhg@ipa.fhg.de>
2017-03-15 16:52:30 +0100

    test travis config

--- .travis.install.sh
+++ .travis.install.sh
@@ -2,4 +2,4 @@ set -e
 set -v
 
 while true; do echo "BEFORE INSTALL IS RUNNING" && sleep 60; done&
-sudo echo "deb http://doc.openrobotino.org/download/packages/amd64 ./" >> /etc/apt/sources.list
+sudo echo "deb http://doc.openrobotino.org/download/packages/amd64 ./" | sudo tee -a /etc/apt/sources.list
