c54b2c14e78dbc4aec8bab335d6aa1d32e70539d
ipa-nhg <nhg@ipa.fhg.de>
2017-07-24 10:27:33 +0200

    install ft17 manually

--- .travis.install.sh
+++ .travis.install.sh
@@ -5,3 +5,5 @@ set -v
 wget http://doc.openrobotino.org/download/packages/amd64/robotino-api2_0.9.16_amd64.deb -O /tmp/robotino.deb
 dpkg -i /tmp/robotino.deb
 
+apt-get install dpkg
+dpkg -i $CATKIN_WORKSPACE/src/ft17_driver/ft17driver_0.0-1_x86_64.deb
