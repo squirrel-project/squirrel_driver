4fef2815343f6afb6184c4e279ee4594111dd13a
Thomas Fäulhammer <faeulhammer@acin.tuwien.ac.at>
2017-09-28 15:26:50 +0200

    version found in kenny raspberry

--- squirrel_interaction/resources/autostart/squirrel_interaction.sh
+++ squirrel_interaction/resources/autostart/squirrel_interaction.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-export ROS_MASTER_URI=http://10.42.0.1:11311
-export ROS_IP=10.42.0.2
+export ROS_MASTER_URI=http://robotino:11311
 source /opt/ros/indigo/setup.bash
 source /home/pi/catkin_ws/devel/setup.bash
 roslaunch squirrel_interaction squirrel_interaction.launch
